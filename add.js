d0ade9ea59c49475f941ee6b3bdc592047ee26e5
Daytuns <daytonbaldizon@gmail.com>
2025-04-29 03:52:19 -0600

    finished

--- add.js
+++ add.js
@@ -2,22 +2,69 @@ const tasks_container = document.getElementById('tasks');
 const submit = document.getElementById('submit');
 const task = document.getElementById('task');
 
-
 submit.addEventListener('click', (event) => {
     event.preventDefault();
-
+  
+    if (task.value.trim() === '') {
+      return;
+    }
+  
     const li = document.createElement('li');
-    const checkbox = document.createElement('input');
-    const del_button = document.createElement('button');
+    const label = document.createElement('label');
+    label.classList.add('container');
 
-    checkbox.type = 'checkbox';
-    del_button.textContent = 'Delete';
+    const input = document.createElement('input');
+    input.type = 'checkbox';
 
-    li.textContent = task.value + ' ';
-    li.append(checkbox);
-    li.append(del_button);
+    const checkmark = document.createElement('div');
+    checkmark.classList.add('checkmark');
+
+    label.appendChild(input);
+    label.appendChild(checkmark);
+
+    const taskText = document.createElement('span');
+    taskText.textContent = task.value;
+    taskText.classList.add('task-text');
+
+    const contentWrapper = document.createElement('div');
+    contentWrapper.classList.add('task-wrapper');  
+    contentWrapper.appendChild(label);
+    contentWrapper.appendChild(taskText);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+    deleteButton.id = 'deleteButton';
+
+    deleteButton.addEventListener('click', () => {
+    li.remove();
+    });
+
+    li.appendChild(contentWrapper);
+    li.appendChild(deleteButton);
     tasks_container.appendChild(li);
 
-});
+  
+    task.value = '';
+    saveData();
 
+  });
+
+  function saveData(){
+    localStorage.setItem("data", tasks_container.innerHTML);
+  }
+
+  function showTask() {
+    tasks_container.innerHTML = localStorage.getItem("data");
+
+    const deleteButtons = document.querySelectorAll('#deleteButton');
+    deleteButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        button.parentElement.remove();
+        saveData();  // Save updated list
+      });
+    });
+  }
+
+  showTask();
+  
 
